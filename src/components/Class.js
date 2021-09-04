678a9724546518135b28ae75302ae7559e5cea9c
Brock Mortenson <mortensonbrock@gmail.com>
2021-09-04 14:51:07 -0600

    second commit

--- src/components/Class.js
+++ src/components/Class.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 class Class extends Component {
+    constructor() {
+        super();
+        this.state = {
+            firstName: ''
+        }
+    }
+
+    handleClick = () => {
+        if (this.state.firstName === '') {
+            this.setState({ firstName: 'Bob' })
+        } else {
+            this.setState({ firstName: '' })
+        }
+    }
+    
     render() {
         return (
             <div>
                 Class Component
+                <p>Name: {this.state.firstName}</p>
+                <button onClick={this.handleClick}>Click Me</button>
             </div>
         );
     }
--- src/components/Functional.js
+++ src/components/Functional.js
@@ -1,7 +1,21 @@
+import React, { useState } from 'react';
+
 function Functional() {
+    const [ name, setName ] = useState('');
+
+    const handleClick = () => {
+        if (name === '') {
+            setName('John');
+        } else {
+            setName('');
+        }
+    }
+    
     return (
         <div>
             Functional
+            <p>Name: {name}</p>
+            <button onClick={handleClick}>Click Me</button>
         </div>
     );
 }
